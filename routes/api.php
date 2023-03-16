f917fb069fbe6351540225dca79375e6f5141586
Mohsin Shah <m.shah@blinkpayment.co.uk>
2023-03-16 11:23:52 +0000

    Added auth and controllers

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,14 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CustomerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/auth/register', 'register');
+    Route::post('/auth/login', 'login');
 });
+
+Route::apiResource('customers', CustomerController::class)->middleware('auth:sanctum');
+Route::apiResource('orders', OrderController::class)->middleware('auth:sanctum');
