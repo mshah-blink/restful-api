a733ed1755efebfc99c2909aeda84874e9bdc451
Mohsin Shah <m.shah@blinkpayment.co.uk>
2023-04-27 07:53:15 +0100

    Added allowed origins

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['*'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['*', 'https://127.0.0.1:3000', 'https://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:3000'],
 
     'allowed_origins_patterns' => [],
 
